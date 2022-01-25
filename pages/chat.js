c227efa3da29497670f8f73367370a7f8abc2d73
Bruno Pinho <brunopinho@outlook.com>
2022-01-25 18:52:28 -0300

    ADD: chat page

---
+++ pages/chat.js
@@ -0,0 +1,13 @@
+import { Text } from '@skynexui/components';
+
+export default function Chat () {
+  return (
+    <>
+    <Text
+      variant="body1"
+    >
+      Página do Chat
+    </Text>
+  </>
+  )
+}
