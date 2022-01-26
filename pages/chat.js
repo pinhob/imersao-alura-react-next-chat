8fff38758e95e4be2995328359893bb8960ad815
Bruno Pinho <brunopinho@outlook.com>
2022-01-26 16:58:47 -0300

    ADD: estrutura inicial da página chat

--- pages/chat.js
+++ pages/chat.js
@@ -1,13 +1,117 @@
-import { Text } from '@skynexui/components';
+import { Box, Text, Button, TextField } from '@skynexui/components';
+import appConfig from '../config.json';
 
 export default function Chat () {
   return (
-    <>
-    <Text
-      variant="body1"
+    <Box
+      styleSheet={{
+        display: 'flex', alignItems: 'center', justifyContent: 'center',
+        backgroundColor: appConfig.theme.colors.primary[500],
+        backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`,
+        backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+        color: appConfig.theme.colors.neutrals['000']
+      }}
     >
-      Página do Chat
-    </Text>
-  </>
+      <Box
+        styleSheet={{
+          display: 'flex',
+          flexDirection: 'column',
+          flex: 1,
+          boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+          borderRadius: '5px',
+          backgroundColor: appConfig.theme.colors.neutrals[700],
+          height: '100%',
+          maxWidth: '95%',
+          maxHeight: '96vh',
+          padding: '32px'
+        }}
+      >
+        <Header />
+        <Box
+          styleSheet={{ 
+            position: 'relative',
+            display: 'flex',
+            flex: 1,
+            height: '80%',
+            backgroundColor: appConfig.theme.colors.neutrals[600],
+            flexDirection: 'column',
+            borderRadius: '5px',
+            padding: '16px',
+          }}
+        >
+          <MessageList />
+
+          <Box
+            as="form"
+            styleSheet={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <TextField
+              // value=''
+              onChange={() => console.log('aplicar lógica')}
+              onKeyPress={() => console.log('aplicar lógica')}
+              placeholder='Insira sua mensagem aqui...'
+              type='textarea'
+              styleSheet={{
+                width: '100%',
+                border: '0',
+                resize: 'none',
+                borderRadius: '5px',
+                padding: '6px 8px',
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                marginRight: '12px',
+                color: appConfig.theme.colors.neutrals[200],
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   )
+}
+
+function Header() {
+  return (
+    <>
+      <Box
+        styleSheet={{ 
+          width: '100%',
+          marginBottom: '16px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Text variant='heading3'>
+          Chat
+        </Text>
+        <Button
+          variant='tertiary'
+          colorVariant='neutral'
+          label='Logout'
+          href='/'
+        />
+      </Box>
+    </>
+  )
+}
+
+function MessageList({ messages }) {
+  return(
+    <Box
+      tag="ul"
+      styleSheet={{
+        overflow: 'scroll',
+        display: 'flex',
+        flexDirection: 'column-reverse',
+        flex: 1,
+        color: appConfig.theme.colors.neutrals["000"],
+        marginBottom: '16px',
+      }}
+    >
+      {/* adicionar lógica e estrutura das mensagens */}
+    </Box>
+  );
 }
